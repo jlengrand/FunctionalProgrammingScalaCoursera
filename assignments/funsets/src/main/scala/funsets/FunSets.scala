31e2df595c5a43a3510f0ccfa2cec630ae889b71
julien Lengrand-Lambert <julien@lengrand.fr>
2017-03-25 11:17:19 +0100

    Implements the filter function

--- assignments/funsets/src/main/scala/funsets/FunSets.scala
+++ assignments/funsets/src/main/scala/funsets/FunSets.scala
@@ -41,12 +41,15 @@ object FunSets {
    * Returns the difference of the two given sets,
    * the set of all elements of `s` that are not in `t`.
    */
-    def diff(s: Set, t: Set): Set = ???
+    def diff(s: Set, t: Set): Set =
+      x => contains(s, x) && !contains(t, x)
   
   /**
    * Returns the subset of `s` for which `p` holds.
    */
-    def filter(s: Set, p: Int => Boolean): Set = ???
+    def filter(s: Set, p: Int => Boolean): Set =
+      x => p(x)
+
   
 
   /**
